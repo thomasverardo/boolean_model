401cf14ca40e3e0cfd363c26637ca1b857d814fa
Thomas <thomas.verardo1@gmail.com>
2023-05-11 12:19:37 +0200

    without stemmer

---
+++ indexV4.py
@@ -0,0 +1,390 @@
+import string
+# Ricordarsi che per usare queste librerie nltk, bisogna installare i database, che vengono salvati in /home/thomas/nltk_data...
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+
+
+
+stop_words = set(stopwords.words("english"))
+punctuation = set(string.punctuation)
+stemmer = PorterStemmer()
+
+
+     
+
+class Node:
+    def __init__(self, term: string, doc_id: int):
+        self.term = term
+        self.posting = [doc_id]
+        self.permutation = tuple() # forse serve solo per root e non root_inv
+        self.left = None
+        self.right = None
+
+    def permutation_words(self):
+        perm = list()
+        word = self.term + "$"
+        perm.append(word)
+        for i in range(1, len(word)):
+            word_1 = word[i:]
+            word_2 = word[0:i]
+            word_3 = word_1 + word_2
+            perm.append(word_3)
+        
+        self.permutation = tuple(perm)
+        return tuple(perm)
+
+
+class Index:
+
+    def __init__(self, documents: list):
+        self.inverted_index = {}
+        self.n_docs = 0
+        self.root = None
+        self.root_inv = None
+        self._build_inverted_index(documents)
+
+    def __str__(self):
+        return str(self.inverted_index)
+    
+    
+    
+    def print_inorder(self):
+        node = self.root
+        return self._inorder(node)
+
+    def _inorder(self, node):
+        if node is not None:
+            l = self._inorder(node.left)
+            m = node.term + ", "
+            r = self._inorder(node.right)
+            return l+m+r
+        else:
+            return ''
+
+
+    def insert(self, node, key, doc_id):
+        
+        if node is None:
+            new_node = Node(key, doc_id)
+            # Creation of permutation index for wildcard queries in the word
+            new_node.permutation_words()
+            return new_node
+        
+        if key < node.term:
+            node.left = self.insert(node.left, key, doc_id)
+        else:
+            node.right = self.insert(node.right, key, doc_id)
+        
+        return node
+    
+    def insert_inv(self, node, key, doc_id):
+        """
+        Create this second method used to insert only in the inverted bst
+        In this way I will create the permutation index only in the rigth index
+        """
+        
+        if node is None:
+            return Node(key, doc_id)
+        
+        if key < node.term:
+            node.left = self.insert_inv(node.left, key, doc_id)
+        else:
+            node.right = self.insert_inv(node.right, key, doc_id)
+        
+        return node
+    
+    
+    def search_posting(self, term, node):
+
+        while node:
+            if term == node.term:
+                return node.posting
+            elif term < node.term:
+                node = node.left
+            else:
+                node = node.right
+        return None
+    
+    
+    def _add_doc_id(self, node, term, i):
+        
+        if node.term == term:
+            node.posting.append(i)
+        
+        elif term < node.term:
+            node.left = self._add_doc_id(node.left, term, i)
+        else:
+            node.right = self._add_doc_id(node.right, term, i)
+
+        return node
+        
+
+
+    def _build_inverted_index(self, documents: list):
+
+        # Sarebbe da sortare tutto l'array di documents e partire dal mediano
+        # In questo modo si costruisce un albero bilanciato
+        # Usare list.sort() tha as O(nlogn) time complexity
+
+        for doc_id, doc in enumerate(documents):
+            i = 0 #It's the position int he document of the term
+            for term in word_tokenize(doc):   # also doc.split() can be used but it's difficult to caputure punctuation
+                
+                term = term.casefold() #lowercase, more aggressive that lower()
+                term_split = term.split('\'')  # split apostrophe words and then save only important words
+                for term_no_ap in term_split:
+
+                        #is already in the index this word?
+                        if self.inverted_index.get(term_no_ap) is not None:
+                            # term is already inserted into the bst
+                            # So we have to serach and modify his posting list
+                            self.root = self._add_doc_id(self.root, term_no_ap, doc_id)
+                            self.root_inv = self._add_doc_id(self.root_inv, term_no_ap[::-1], doc_id)
+
+                            if self.inverted_index[term_no_ap].get(doc_id) is not None:
+                                self.inverted_index[term_no_ap][doc_id].append(i) 
+                            else:
+                                self.inverted_index[term_no_ap][doc_id] = [i]
+
+                        else:
+
+                            # Now insert on the binary tree 
+                            self.root = self.insert(self.root, term_no_ap, doc_id)
+                            self.root_inv = self.insert_inv(self.root_inv, term_no_ap[::-1], doc_id)
+                            
+                            self.inverted_index[term_no_ap] = {}
+                            self.inverted_index[term_no_ap][doc_id] = [i]
+
+
+                i += 1
+        self.n_docs = len(documents)
+
+        return self.inverted_index
+    
+
+    def add_doc(self, doc: string):
+
+        doc_id = self.n_docs
+        i = 0
+        for term in word_tokenize(doc):   # also doc.split() can be used but it's difficult to caputure punctuation
+                
+            term = term.casefold() #lowercase, more aggressive that lower()
+            term_split = term.split('\'')  # split apostrophe words and then save only important words
+            for term_no_ap in term_split:
+
+                    #is already in the index this word?
+                    if self.inverted_index.get(term_no_ap) is not None:
+                        # term is already inserted into the bst
+                        # So we have to serach and modify his posting list
+                        self.root = self._add_doc_id(self.root, term_no_ap, doc_id)
+                        self.root_inv = self._add_doc_id(self.root_inv, term_no_ap[::-1], doc_id)
+
+                        if self.inverted_index[term_no_ap].get(doc_id) is not None:
+                            self.inverted_index[term_no_ap][doc_id].append(i) 
+                        else:
+                            self.inverted_index[term_no_ap][doc_id] = [i]
+
+                    else:
+
+                        # Now insert on the binary tree 
+                        self.root = self.insert(self.root, term_no_ap, doc_id)
+                        self.root_inv = self.insert_inv(self.root_inv, term_no_ap[::-1], doc_id)
+                        
+                        self.inverted_index[term_no_ap] = {}
+                        self.inverted_index[term_no_ap][doc_id] = [i]
+
+            i += 1        
+
+        self.n_docs = doc_id + 1
+
+
+    def boolean_query(self, query_string):
+
+        # Use set intersection, union and difference because more performant 
+
+        split_query = [term.casefold() for term in query_string.split(" ")]
+        all_docs = set([d for d in range(self.n_docs)])
+
+        try:
+
+            if split_query[0].casefold() == "not":
+                res = all_docs.difference(self.inverted_index[split_query[1]].keys())
+            else:
+                res = set(self.inverted_index[split_query[0]].keys())
+            for i in range(len(split_query)):
+                if split_query[i].lower() == "and":
+                    if split_query[i+1].lower() == "not":
+                        not_doc = all_docs.difference(self.inverted_index[split_query[i+2]].keys())
+                        res = res.intersection(not_doc)
+                    else:
+                        res = res.intersection(self.inverted_index[split_query[i+1]].keys())
+
+                elif split_query[i].lower() == "or":
+                    if split_query[i+1].lower() == "not":
+                        not_doc = all_docs.difference(self.inverted_index[split_query[i+2]].keys())
+                        res = res.union(not_doc)
+                    else:
+                        res = res.union(self.inverted_index[split_query[i+1]].keys())
+
+            return res
+        except KeyError:
+            print("Term is not present")
+            return list()
+    
+    
+    # per forza si usa la dictionary because ha le position
+    def _phrase_query(self, terms):
+        """
+        Private method to perform a phrase query on the inverted index.
+        Given a list of terms, returns a list of documents that contain all of the terms in the specified order.
+        """
+        try:
+            docs = self.inverted_index[terms[0]]
+            for term in terms[1:]:
+                new_docs = {}
+                postings = self.inverted_index[term]
+                for doc_id, positions in postings.items():
+                    if doc_id in docs:
+                        prev_positions = docs[doc_id]
+                        new_positions = []
+                        i = 0
+                        j = 0
+                        while i < len(prev_positions) and j < len(positions):
+                            if positions[j] == prev_positions[i] + 1:
+                                new_positions.append(positions[j])
+                                i += 1
+                                j += 1
+                            elif positions[j] > prev_positions[i] + 1:
+                                i += 1
+                            else:
+                                j += 1
+                        if new_positions:
+                            new_docs[doc_id] = new_positions
+                docs = new_docs
+            return list(docs.keys())
+        except KeyError:
+            print("Term is not present")
+            return list()
+
+    
+    def phrase_query(self, query):
+        terms = [term.casefold() for term in query.split(" ")]
+        return self._phrase_query(terms)
+
+    
+    def edit_distance(self, u, v):
+        """
+        Computes the Levenshtein distance between two strings.
+        Returns the minimum number of insertions, deletions, and substitutions required to transform u into v.
+        """
+        nrows = len(u) + 1
+        ncols = len(v) + 1
+        M = [[0] * ncols for i in range(0, nrows)]
+        for i in range(0, nrows):
+            M[i][0] = i
+        for j in range(0, ncols):
+            M[0][j] = j
+        for i in range(1, nrows):
+            for j in range(1, ncols):
+                candidates = [M[i-1][j] + 1, M[i][j-1] + 1]
+                if u[i-1] == v[j-1]:
+                    candidates.append(M[i-1][j-1])
+                else:
+                    candidates.append(M[i-1][j-1] + 1)
+                M[i][j] = min(candidates)
+        return M[-1][-1]
+
+    def find_nearest(self, word, dictionary, keep_first=False):
+        """
+        Finds the closest match to a word in a dictionary using the Levenshtein distance.
+        If keep_first is True, only considers words in the dictionary that start with the same letter as the word.
+        """
+        if keep_first:
+            dictionary = list(filter(lambda w: w[0] == word[0], dictionary))
+        distances = map(lambda x: self.edit_distance(word, x), dictionary) # [1, 3, 2]
+        return min(zip(distances, dictionary))[1] # [(1, 'hello'), (2, ...) ] 
+    
+    def phrase_query_sc(self, query):
+        """
+        Performs a phrase query on the inverted index.
+        Given a query string, returns a list of documents that contain all of the words in the specified order.
+        If a word is not found in the inverted index, suggests the closest match using the Levenshtein distance.
+        """
+        terms = [term.casefold() for term in query.split(" ")]
+        dictionary = list(self.inverted_index.keys())
+        postings = []
+        for term in terms:
+            if term in dictionary:
+                postings.append(term)
+            else:
+                sub = self.find_nearest(term, dictionary, keep_first=True)
+                print("{} not found. Did you mean {}?".format(term, sub))
+                postings.append(sub)
+        
+        return self._phrase_query(postings)
+        
+
+    
+
+    
+    def wildcard_query(self, query):
+
+        inv = -1
+
+        if query[0] == "*":
+            r_part = query[::-1][:-1]
+            node = self.root_inv
+            inv = 1
+        elif query[-1] == "*":
+            r_part = query[:-1]
+            node = self.root
+            inv = 0
+        
+        if inv == 1 or inv == 0:
+            while node:
+                term = node.term[:len(r_part)]
+                if term == r_part:
+                    # Thanks to this, we start searching only in the subtree starting from node
+                    return self._wildcard(node, r_part, [], inv)
+                elif r_part < term:
+                    node = node.left
+                else:
+                    node = node.right
+
+        else:
+            # permuted index wildcard query
+            # rotate the word to have the wildcard at last
+            query += "$"
+            i = query.find("*")
+            r_part = query[i+1:] + query[:i]
+            node = self.root
+            inv = -1
+            return self._wildcard(node, r_part, [], inv)
+
+
+        return None
+
+    def _wildcard(self, node, r_part, full_terms, inv):
+
+        if node is not None:
+            
+            full_terms = self._wildcard(node.left, r_part, full_terms, inv)
+            full_terms = self._wildcard(node.right, r_part, full_terms, inv)
+            
+            if inv == -1: # if "a*b"
+                for perm in node.permutation:
+                    if perm[:len(r_part)] == r_part:
+                        full_terms.append(node.term)
+                        return full_terms
+
+            else:
+                if node.term[:len(r_part)] == r_part:
+                    if inv == 1: # if "*bc"
+                        full_terms.append(node.term[::-1])
+                    if inv == 0: # if "ab*"
+                        full_terms.append(node.term)
+                    return full_terms
+            
+        
+        return full_terms
---
+++ projectV4.py
@@ -0,0 +1,185 @@
+import time
+from indexV4 import Index
+import pickle
+import numpy as np
+from cisi import CISI
+
+
+"""
+    - Implementare il nasted dictionary -- FATTO
+
+        {
+        "term1": {
+            1: [2, 5],
+            2: [1, 7, 9]
+        },
+        "term2": {
+            1: [3, 8],
+            3: [2]
+        },
+        "term3": {
+            2: [4],
+            3: [1, 6]
+        }
+    }
+
+
+    - We don't use hash table because with hash we can't look for prefixes --FATTO
+
+    - maintaining multiple indexes can make insertion and modification more complex, 
+        as you have to update both indexes. However, the tradeoff is often worth it 
+        in terms of improved query performance.
+        Inverted indexes are optimized for efficient querying, so it's usually 
+        more important to optimize for fast search rather than fast insertion/modification.
+
+    -  For terms with a large number of distinct postings lists, the hash table 
+        can be replaced with a balanced tree to reduce the memory footprint and improve 
+        lookup times.
+
+    - my_dict = {
+        ('my_key', [1, 2]):
+            2: [4],
+            3: [1, 6]
+            }
+
+
+        now= {
+            ('my', ('my$', 'y$m', '$my')):
+                2: [4],
+                3: [1, 6]
+        }
+        This will be the structure of our permuted index
+
+
+    - phrase_query, the method implemented in class that works with indexes i and j, performs as my 
+        method that uses intersection and union method; my method is easly to understand
+
+
+    - When constructing bst, we have to start from the middle of all words.
+
+"""
+
+
+def timeit_test(fun, times):
+    start = time.time()
+    for i in range(times):
+        fun
+    end = time.time()
+    finish = (end-start)/times
+    return finish
+
+
+def read_files(file_names):
+
+    documents = []
+    try:
+        for file in file_names:
+            with open("data/"+file, "r") as f:
+                documents.append(f.read())
+            f.close()
+    except:
+        print("File doesn't exist")
+    
+    return documents
+    
+
+
+#           MAIN
+
+
+
+# with open("data/lorem_ipsum", "r") as f:
+#     documents = [line.split("\n")[0] for line in f]
+# f.close()
+
+
+# indexes = Index(documents)
+
+# print(indexes.print_inorder())
+
+# new_doc = "Ehi, how it is my friend Giovanni? I like his cats"
+
+# # indexes.add_doc(new_doc)
+
+
+# query = "semper AND commodo OR accumsan"
+
+# print("Result of query '", query, "': ", indexes.boolean_query(query))
+
+# query_phrase = "Lorem ipsum dolor sit"
+# print("Result of query prof '",query_phrase,"': ",indexes.phrase_query(query_phrase))
+
+# # # print("Time for query: ", timeit_test(inverted_doc.query(query), 100000000))
+# # # print("Time for query phrase: ", timeit_test(inverted_doc.phrase_query(query_phrase), 100000000))
+
+
+# query_wrong = "lorum ipsimum dolor sut"
+# print("Result of query '",query_wrong, "': ", indexes.phrase_query_sc(query_wrong))
+# # # print("Time for query with sc: ", timeit_test(inverted_doc.phrase_query_sc(query_wrong), 100000))
+
+
+# wildcard_query = "tem*"
+# print(indexes.wildcard_query(wildcard_query))
+
+# print("aaa")
+
+
+
+
+
+
+
+# # file_name = ["middlemarch", "Romeo and Juliet", "moby dick", "A room with a view"]
+# file_name = ["moby dick"]
+# documents = read_files(file_name)
+
+# cicero = Index(documents)
+
+# print(cicero.boolean_query("Romeo and juliet"))
+
+# print(cicero.phrase_query("they saw many whales sporting in the ocean"))
+# # print(cicero.phrase_query("they saw many whales"))
+# print(cicero.wildcard_query("div*"))
+
+# new_doc = "Ehi, how it is my friend Giovanni? I like his cats"
+
+# cicero.add_doc(new_doc)
+
+
+
+
+
+
+
+
+
+# with open("data/articles2.pkl", "rb") as f:
+#     articles = pickle.load(f)
+
+# with open("data/queries2.pkl", "rb") as f:
+#     queries = pickle.load(f)
+
+# with open("data/relevance.pkl", "rb") as f:
+#     relevance = pickle.load(f)
+
+# bol = Index(articles)
+# print("Query: ",queries[0], " --> ",bol.phrase_query(queries[0]))
+
+
+cisi = CISI()
+## Here we check some statistics and info of CISI dataset
+
+print('Read %s documents, %s queries and %s mappings from CISI dataset' % 
+      (len(cisi.doc_set), len(cisi.qry_set), len(cisi.rel_set)))
+
+number_of_rel_docs = [len(value) for key, value in cisi.rel_set.items()]
+print('Average %.2f and %d min number of relevant documents by query ' % 
+      (np.mean(number_of_rel_docs), np.min(number_of_rel_docs)))
+
+print('Queries without relevant documents: ', 
+      np.setdiff1d(list(cisi.qry_set.keys()),list(cisi.rel_set.keys())))
+
+
+
+
+print("ciao")
